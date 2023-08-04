70abcc2c9b171cdaf276a47f2e6a2981d7779897
CapitanDagestan <aleskerovbogdan@gmail.com>
2023-08-04 20:43:23 +0300

    Third message

--- src/ProgrammLesson4.java
+++ src/ProgrammLesson4.java
@@ -2,5 +2,6 @@ public class ProgrammLesson4 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git from Bogdan");
+        System.out.println("Hello Git from our group");
     }
 }
