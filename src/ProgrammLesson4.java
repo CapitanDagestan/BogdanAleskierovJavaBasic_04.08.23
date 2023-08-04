e43141026dfc478adfe1c7814643c028e83ccfa2
CapitanDagestan <aleskerovbogdan@gmail.com>
2023-08-04 20:27:50 +0300

    Second message

--- src/ProgrammLesson4.java
+++ src/ProgrammLesson4.java
@@ -1,5 +1,6 @@
 public class ProgrammLesson4 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git  ffrom Bogdan");
     }
 }
